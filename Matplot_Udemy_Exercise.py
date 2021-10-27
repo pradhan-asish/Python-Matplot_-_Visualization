fd8b3e20ce65f2b874a35292dc8e4c262345edb9
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-27 23:14:56 +0530

    Modification done in the file

--- Matplot_Udemy_Exercise.py
+++ Matplot_Udemy_Exercise.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x = np.arange(0,100)
 y = x *2
 z = x **2
-print('Hi I am Asish')
+print('Hi I am Asish.')
 plt.show()
 fig = plt.figure()
 ax = fig.add_axes([0,0,1,1])
